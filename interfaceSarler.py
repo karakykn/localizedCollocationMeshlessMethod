4678d993a1359d9e48e20a8f797cb32c73ed510d
navierchokes <93181783+navierchokes@users.noreply.github.com>
2021-10-26 09:55:33 +0300

    Update interfaceSarler.py

--- interfaceSarler.py
+++ interfaceSarler.py
@@ -34,60 +34,26 @@ df.to_csv('records/results_intefraceOuter.csv', index=False)
 df = pd.DataFrame(np.array([solution.solnInner, solution.mesh.interfaceLocs[:,0], solution.mesh.interfaceLocs[:,1]]).T, columns=['Approx. soln.','x','y'])
 df.to_csv('records/results_intefraceInner.csv', index=False)
 
-"""Plotting the nodes"""
-fig = plt.figure()
-ay = fig.gca()
-ay.plot(solution.mesh.locations[:,0],solution.mesh.locations[:,1],'x',color='blue')
-for i in range(solution.mesh.locations.shape[0]):
-    ay.text(solution.mesh.locations[i,0]+.01, solution.mesh.locations[i,1]-.05, str(i),color='blue')
-ay.plot(solution.mesh.interfaceLocs[:,0],solution.mesh.interfaceLocs[:,1],'o',color='red')
-for i in range(solution.mesh.interfaceLocs.shape[0]):
-    ay.text(solution.mesh.interfaceLocs[i,0]-.01, solution.mesh.interfaceLocs[i,1]+.05, str(i),color='red')
+# """Plotting the nodes"""
+# fig = plt.figure()
+# ay = fig.gca()
+# ay.plot(solution.mesh.locations[:,0],solution.mesh.locations[:,1],'x',color='blue')
+# for i in range(solution.mesh.locations.shape[0]):
+#     ay.text(solution.mesh.locations[i,0]+.01, solution.mesh.locations[i,1]-.05, str(i),color='blue')
+# ay.plot(solution.mesh.interfaceLocs[:,0],solution.mesh.interfaceLocs[:,1],'o',color='red')
+# for i in range(solution.mesh.interfaceLocs.shape[0]):
+#     ay.text(solution.mesh.interfaceLocs[i,0]-.01, solution.mesh.interfaceLocs[i,1]+.05, str(i),color='red')
 
 # """Plotting the approximated solution"""
-fig1 = plt.figure()
-ax = fig1.gca(projection='3d')
-tess = scipy.spatial.Delaunay(solution.mesh.locations)
-x = tess.points[:, 0]
-y = tess.points[:, 1]
-tri = tess.vertices # or tess.simplices depending on scipy version
-triang = mtri.Triangulation(x=solution.mesh.locations[:, 0], y=solution.mesh.locations[:, 1], triangles=tri)
-ax.plot_trisurf(triang, solution.solnOuter)
-plt.xlabel('x')
-plt.ylabel('y')
-ax.set_zlim(0,2)
-plt.show()
-
-
-# plt.plot(mesh.locations[:,0],mesh.locations[:,1],'o')
-# plt.xlim([-1,1])
-# plt.ylim([-1,1])
-
-# n = np.zeros((mesh.interfaceBNN,2))
-# for i in range(int(mesh.interfaceBNN/2)):
-#     tt = i*np.pi/16
-#     n[i,1] = -3*.65*(np.cos(tt))**2*np.sin(tt)
-#     n[i,0] = -3*.65*(np.sin(tt))**2*np.cos(tt)
-#
-#     plt.plot([mesh.interfaceLocs[i,0], mesh.interfaceLocs[i,0]+n[i,0]], [mesh.interfaceLocs[i,1], mesh.interfaceLocs[i,1]+n[i,1]])
-#
+# fig1 = plt.figure()
+# ax = fig1.gca(projection='3d')
+# tess = scipy.spatial.Delaunay(solution.mesh.locations)
+# x = tess.points[:, 0]
+# y = tess.points[:, 1]
+# tri = tess.vertices # or tess.simplices depending on scipy version
+# triang = mtri.Triangulation(x=solution.mesh.locations[:, 0], y=solution.mesh.locations[:, 1], triangles=tri)
+# ax.plot_trisurf(triang, solution.solnOuter)
+# plt.xlabel('x')
+# plt.ylabel('y')
+# ax.set_zlim(0,2)
 # plt.show()
-
-# plt.plot(mesh.locations[:,0],mesh.locations[:,1],'o')
-# plt.xlim([-1,1])
-# plt.ylim([-1,1])
-#
-# n = np.zeros((mesh.interfaceBNN,2))
-# for i in range(int(mesh.interfaceBNN/2)):
-#     tt = i*np.pi/16
-#     if tt<np.pi/2 or (tt>=np.pi and tt<3*np.pi/2):
-#         geomCorrector = -1
-#     else:
-#         geomCorrector = 1
-#     n[:,0] = geomCorrector*np.sin(tt)
-#     n[:,1] = geomCorrector*np.cos(tt)
-#
-#     plt.plot([mesh.interfaceLocs[i,0], mesh.interfaceLocs[i,0]+n[i,0]], [mesh.interfaceLocs[i,1], mesh.interfaceLocs[i,1]+n[i,1]])
-#
-# plt.show()
-
